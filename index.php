dc39874a77413b1fa7ae81bfa80b033d3cbb8420
Todd Bennett <todd.m.bennett@gmail.com>
2017-02-03 14:57:47 +0900

    Updated correct link

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 <img src='/images/kitten.jpg' alt='Adorable kitten' />
 
-<a href="http://wikipedia.org/kittens">Learn about kittens on wikipedia</a>
+<a href="http://wikipedia.org/kitten">Learn about kittens on wikipedia</a>
